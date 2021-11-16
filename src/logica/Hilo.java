a4db518fd21e503e67be2d03d07c6fa00fb64540
Carlos <carlos@sofiri.com>
2021-11-16 10:39:12 -0500

    ADD: server interpretando los datos enviados

--- src/logica/Hilo.java
+++ src/logica/Hilo.java
@@ -1,8 +1,7 @@
 
 package logica;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import com.google.gson.*;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -13,17 +12,20 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
+import org.json.*;
 
 
 public class Hilo extends Thread{
 
     private Socket host;
     private DataOutputStream datosSalida;
-    private BufferedReader datosEntrada;
+    private DataInputStream input_stream;
+    private BufferedReader datosEntrada, inputStream;
     public static int NUM_CLIENTES = 0;
     private int clienteNo, salir=0;
     protected String mensajeRecibido;
     private Cliente cliente;
+    byte buffer[];  // Para almacenar lo que llegue del cliente
     
     
     public Cliente getCliente() {
@@ -41,85 +43,86 @@ public class Hilo extends Thread{
         host = c;
         NUM_CLIENTES++;
         clienteNo = NUM_CLIENTES;
+        // Buffer input stream
+        try { 
+        input_stream=new DataInputStream(host.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }         
     }
 
     @Override
     public void run() {
-        String mensaje = "Hola "+ clienteNo + "!";
-        
-        
-        try {
-            
-            //SE LEE LA ENTRADA
-            datosEntrada = new BufferedReader(new InputStreamReader(host.getInputStream()));
-            while((mensajeRecibido = datosEntrada.readLine()) != null && salir == 0 ) //Mientras haya mensajes desde el cliente
-            {
-                    try {
-                        Gson gson = new Gson();
-                        Json json = gson.fromJson(mensajeRecibido, Json.class);
-                        
-                        switch (json.getPeticion()) {
-                            case "connect": 
-                                mensaje = "Cliente Conectado";
-                                datosSalida = new DataOutputStream(host.getOutputStream());
-                                datosSalida.write(mensaje.getBytes());
-                            break;
-                            case "disconnect":  
-                                System.out.println("Cliente Desconectado");
-                                mensaje = "Cliente Desconectado";
-                                datosSalida = new DataOutputStream(host.getOutputStream());
-                                datosSalida.write(mensaje.getBytes());
-                                salir=1;
-                                datosSalida.close();
-                                datosEntrada.close();
-                                host.close();
-                            break;
-                            case "update":
-                                Gson gsonF = new Gson();
-                                ClienteTemp[] clientesTemp = gsonF.fromJson(json.getInfo(),ClienteTemp[].class);
-                                System.out.println("-------------------------------------------");
-                                System.out.println(clientesTemp[0].getCant_semaforos());
-                                
-                                getCliente().setId(clientesTemp[0].getClient_id());
-                                getCliente().setCantSemaforos1(clientesTemp[0].getCant_semaforos());
-                                getCliente().setCantSemaforos2(clientesTemp[1].getCant_semaforos());
-                                getCliente().setCantRojo1(clientesTemp[0].getLuz_red_broken());
-                                getCliente().setCantRojo2(clientesTemp[1].getLuz_red_broken());
-                                getCliente().setCantAmarillo1(clientesTemp[0].getLuz_yellow_broken());
-                                getCliente().setCantAmarillo2(clientesTemp[1].getLuz_yellow_broken());
-                                getCliente().setCantVerde1(clientesTemp[0].getLuz_green_broken());
-                                getCliente().setCantVerde2(clientesTemp[1].getLuz_green_broken());
-                                
-                                ResponseClient[] respuestaCliente = {
-                                new ResponseClient(1, getCliente().isEstRojo1(), getCliente().isEstAmarillo1(), getCliente().isEstVerde1()),
-                                new ResponseClient(2, getCliente().isEstRojo1(), getCliente().isEstAmarillo2(), getCliente().isEstVerde2())
-                            };
-                                 
-
-                                mensaje = gson.toJson(respuestaCliente);
-                                datosSalida = new DataOutputStream(host.getOutputStream());
-                                datosSalida.write(mensaje.getBytes());
-                            break;
-                            default: 
-                                System.out.println("Opcion No Valida");
-                                mensaje = "Opcion No Valida";
-                                datosSalida = new DataOutputStream(host.getOutputStream());
-                                datosSalida.write(mensaje.getBytes());
-                            break;
-                        }
-                    } catch(JsonSyntaxException e){
-                        System.out.println("Json Con Estructura Erronea");
-                    }
+    System.out.println("Hola "+ clienteNo + "!");
+    JSONObject mensaje = new JSONObject();
+    while(!(host.isClosed())){
+        try { 
+                buffer = new byte[1024];
+                input_stream.read(buffer);
+                mensajeRecibido = new String(buffer).trim();
+                if (!(mensajeRecibido.equals(""))) {
+                    JsonElement root = new JsonParser().parse(mensajeRecibido);
+                    String request_type = root.getAsJsonObject().get("peticion").getAsString();
+                    String info = root.getAsJsonObject().get("info").toString();
+                    System.out.println(request_type); 
+                    System.out.println(info);
+                    Gson gson = new Gson();
+                    Json json = new Json(request_type, info);
                     
-                   
-            }
-        } catch (IOException ex) {
-            System.out.println("Mensaje Incorrecto");
-        }        
+                    switch (json.getPeticion()) {
+                        case "connect": 
+                            System.out.println("Cliente Conectado");
+                            mensaje.put("mensaje","Cliente Conectado");
+                            datosSalida = new DataOutputStream(host.getOutputStream());
+                            datosSalida.writeUTF(mensaje.toString());
+                        break;
+                        case "disconnect":  
+                            System.out.println("Cliente Desconectado");
+                            mensaje.put("mensaje","Cliente Desconectado");
+                            datosSalida = new DataOutputStream(host.getOutputStream());
+                            datosSalida.writeUTF(mensaje.toString());
+                            salir=1;
+                            datosSalida.close();
+                            datosEntrada.close();
+                            host.close();
+                        break;
+                        case "update":
+                            Gson gsonF = new Gson();
+                            ClienteTemp[] clientesTemp = gsonF.fromJson(json.getInfo(),ClienteTemp[].class);
+                            System.out.println("-------------------------------------------");
+                            System.out.println(clientesTemp[0].getCant_semaforos());
+                            
+                            getCliente().setId(clientesTemp[0].getClient_id());
+                            getCliente().setCantSemaforos1(clientesTemp[0].getCant_semaforos());
+                            getCliente().setCantSemaforos2(clientesTemp[1].getCant_semaforos());
+                            getCliente().setCantRojo1(clientesTemp[0].getLuz_red_broken());
+                            getCliente().setCantRojo2(clientesTemp[1].getLuz_red_broken());
+                            getCliente().setCantAmarillo1(clientesTemp[0].getLuz_yellow_broken());
+                            getCliente().setCantAmarillo2(clientesTemp[1].getLuz_yellow_broken());
+                            getCliente().setCantVerde1(clientesTemp[0].getLuz_green_broken());
+                            getCliente().setCantVerde2(clientesTemp[1].getLuz_green_broken());
+                            
+                            ResponseClient[] respuestaCliente = {
+                            new ResponseClient(1, getCliente().isEstRojo1(), getCliente().isEstAmarillo1(), getCliente().isEstVerde1()),
+                            new ResponseClient(2, getCliente().isEstRojo1(), getCliente().isEstAmarillo2(), getCliente().isEstVerde2())
+                            };
+                            String data = gson.toJson(respuestaCliente);
+                            datosSalida = new DataOutputStream(host.getOutputStream());
+                            datosSalida.write(data.getBytes());
+                        break;
+                        default: 
+                            System.out.println("Request type No Valida");
+                            mensaje.put("mensaje","Request type No Valida");
+                            datosSalida = new DataOutputStream(host.getOutputStream());
+                            datosSalida.writeUTF(mensaje.toString());
+                        break;
+                    }
         
+                }
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }                   
+    }    
     }
-    
-    
-    
-    
 }
